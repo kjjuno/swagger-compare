51b0e92d391d0211be27333187236fa6d6bb929f
Kevin Johnson <kjohnson@xactware.com>
2019-02-13 10:05:09 -0700

    basic boilerplate stuff

---
+++ src/swagger-compare-cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+var compare = require('./swagger-compare');
+
+var str = compare.hello();
+console.log(str);
---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+
+describe('fake tests', function() {
+  it('is a fake test', function() {
+    assert.equal(1,1);
+  });
+  it('is another fake test', function() {
+    assert.equal(1,1);
+  });
+});
